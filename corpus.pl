378e77cb15bcd221a78504f69a26a8e2ec6867a3
kawasaki-thesis <t14253sk@sfc.keio.ac.jp>
2017-12-21 00:23:17 +0900

    コーパスと１０数値型に配列を変換（未正規化）

--- corpus.pl
+++ corpus.pl
@@ -21,14 +21,23 @@ for($i=0; $i<$fnum; $i++){
 			$j++;
 		}
 	}
-	
 	close(IN);
 }
 
+$wordnum=0;
+for($i=0; $i<$fnum; $i++){
+	for($j=0; $j<=$#{$words[$i]}; $j++){
+		$corpus[$wordnum][0]= '\'' . $words[$i][$j] . '\'';
+		for($k=1; $k<=$fnum; $k++){
+			$corpus[$wordnum][$k]=0;
+		}
+		$corpus[$wordnum][($i+1)]++;
+		$wordnum++;
+	}
+}
 
-
-foreach (@words){
-	print "(" . join(",", @{$_}) . ")\n";
+foreach (@corpus){
+	print "(" . join(", ", @{$_}) . ")\n";
 	#jをカンマで横に並べてi行表示
 }
 
