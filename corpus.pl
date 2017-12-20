bbd481727a011b4af53a9b14289e1daeb51f98b0
kawasaki-thesis <t14253sk@sfc.keio.ac.jp>
2017-12-20 23:10:11 +0900

    data下の複数ファイルを読み込み、単語抽出後配列に格納を確認

--- corpus.pl
+++ corpus.pl
@@ -4,26 +4,34 @@ use utf8;
 
 #binmode(STDOUT,":encoding(utf-8)");
 
-$num = 1;
-
-$inputfile="C:/Users/sayak/work/datashape/test" . $num . ".txt";
-
-open(IN,$inputfile) || die "$!";
-#binmode(IN,":encoding(euc-jp)");
-
-while(<IN>){
-	chomp;
-	@list = split(/\t/);
+for($i=0; $i<5; $i++){
+	$j=0;
+	$inputfile="C:/Users/sayak/work/datashape/data/test" . $i . ".txt";
 	
-	if(@list[1] =~ /NN|VV|JJ|NP/){
-		print $list[2] . "\n";
-		#$meisi[$i] = $list[0];
-		$i++;
+	open(IN,$inputfile) || die "$!";
+	#binmode(IN,":encoding(euc-jp)");
+	
+	while(<IN>){
+		chomp;
+		@list = split(/\t/);
+		
+		if($list[1] =~ /NN|VV|JJ|NP/){
+			$words[$i][$j] = "$list[2]\n";
+			print $words[$i][$j];
+			$j++;
+		}
 	}
+	
+	close(IN);
 }
 
-close(IN);
-
+for($i=0; $i<5; $i++){
+	print $i;
+	print "file------------------\n";
+	foreach (@words[$i]){
+		print "@$_\n";
+	}
+}
 #$text = $meisi[int(rand($i))] . "は" . $meisi[int(rand($i))] . "の上に" . $meisi[int(rand($i))] . "を作らず";
 
 
