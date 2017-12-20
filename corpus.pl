427aa660610977cd01bd1d3f4e6e7cfefb229c79
kawasaki-thesis <t14253sk@sfc.keio.ac.jp>
2017-12-21 02:15:19 +0900

    出来たと思ったけど本番データで動かしたところ明らかに結果がおかしい

--- corpus.pl
+++ corpus.pl
@@ -5,12 +5,12 @@ use utf8;
 #binmode(STDOUT,":encoding(utf-8)");
 
 #データのファイル数＝クラス数（本番は10
-$fnum=5;
+$fnum=10;
 
 #各ファイルから名詞、固有名詞、動詞、形容詞を抽出して格納
 for($i=0; $i<$fnum; $i++){
 	$j=0;
-	$inputfile="C:/Users/sayak/work/datashape/data/test" . $i . ".txt";
+	$inputfile="C:/Users/sayak/work/datashape/data/class" . ($i+1) . ".txt";
 	
 	open(IN,$inputfile) || die "$!";
 	#binmode(IN,":encoding(euc-jp)");
